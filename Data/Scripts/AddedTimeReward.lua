db80e74294a38557f1e6e77305edc1fa52257067
Alexandre de Villeneuve <adv2072@gmail.com>
2020-11-29 19:48:25 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Data/Scripts/AddedTimeReward.lua
@@ -0,0 +1,11 @@
+﻿local addedTimeReward = script:GetCustomProperty("AddedTimeReward"):WaitForObject()
+local distanceTxtBox = addedTimeReward:GetChildren("DistanceTxtBox")
+
+function OnRewardUI(distanceReward)
+    distanceTxtBox.text = string.format(distanceTxtBox .. "for Distance")
+    addedTimeReward.visibility = Visibility.FORCE_ON
+    Task.Wait(10)
+    addedTimeReward.visibility = Visibility.FORCE_OFF
+end
+
+Events.Connect("E_RewardUI", OnRewardUI)
---
+++ Data/Scripts/UnlockController.lua
@@ -0,0 +1,19 @@
+﻿local doubleSpeedUnlock = script:GetCustomProperty("DoublespeedUnlock"):WaitForObject()
+local journalUnlock = script:GetCustomProperty("JournalUnlock"):WaitForObject()
+
+local player = Game.GetLocalPlayer()
+
+function OnResourceChanged(player, resourceId, newValue)
+    if (player:GetResource("level") == 4) then
+        doubleSpeedUnlock.visibility = Visibility.FORCE_ON
+        Task.Wait(4)
+        doubleSpeedUnlock.visibility = Visibility.FORCE_OFF
+       
+        journalUnlock.visibility = Visibility.FORCE_ON
+        Task.Wait(4)
+        journalUnlock.visibility = Visibility.FORCE_OFF
+    end
+end
+
+player.resourceChangedEvent:Connect(OnResourceChanged)
+
