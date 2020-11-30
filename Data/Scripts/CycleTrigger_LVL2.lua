f8f8e3fad0b8da9e066b93c376ff2168725bffe8
Alaa (Alex) <Master_here100@yahoo.com>
2020-11-30 02:11:03 +0200

    movingplatfrom different speeds

---
+++ Data/Scripts/CycleTrigger_LVL2.lua
@@ -0,0 +1,12 @@
+﻿local cycleTrigger_LVL2 = script:GetCustomProperty("CycleTrigger_LVL2"):WaitForObject()
+local cycleTrigger = cycleTrigger_LVL2:FindChildByName("Trigger")
+
+function OnBeginOverlap(trigger, other)
+    if other:IsA("Player")  and not other.isDead then
+        Events.Broadcast("E_CycleDone_LVL2")
+        --Events.BroadcastToAllPlayers("E_CycleDone")
+    end
+end
+
+
+cycleTrigger.beginOverlapEvent:Connect(OnBeginOverlap)
